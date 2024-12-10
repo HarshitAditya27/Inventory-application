0259407495334a2a7200fcfa1d50ada1d8605956
Harshit Aditya <harshitaditya27@gmail.com>
2024-12-10 16:21:39 +0530

    Added redux store functionalities

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,29 @@ const initialState = {
 
 const productSlice = createSlice({
   name: "products",
+  initialState,
+  reducers: {
+    addProducts(state, action) {
+      state.products.push(action.payload);
+    },
+    updateProduct(state, action) {
+      const idx = state.products.findIndex(
+        (product) => product.id == action.payload.id
+      );
+      if (idx !== -1) {
+        state.products[idx] = action.payload;
+      }
+    },
+    deleteProduct(state, action) {
+      state.products = state.products.filter(
+        (curProduct, id) => id !== action.payload
+      );
+    },
+  },
 });
+
+export const { addProduct, updateProduct, deleteProduct } =
+  productSlice.actions;
 
 export const store = configureStore({
   reducer: {
